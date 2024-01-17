fae28f1ecc3244e90ccd1bcac22e4bac2b7ea2fb
alexlofton <147463152+alexlofton@users.noreply.github.com>
2024-01-17 12:20:18 -0700

    inquirer function added to index.js

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const connect = require("./configuration/connection");
 const inquirer = require("inquirer");
+
+
+function startApp() {
+    inquirer.prompt([
+    {
+    name: "prompt",
+    type: "list",
+    message: "What action do you want?",
+    choices: [
+    "View all departments",
+    "View all roles",
+    "View all employees",
+    "Add a new department",
+    "Add a new role",
+    "Add a new employee",
+    "Update employee roles",
+    "Exit"
+    ]
+    }])
+    .then(function (response) {
+    switch (response.prompt) {
+    case "View all departments":
+    viewDepartments();
+    break;
+    case "View all roles":
+    viewRoles();
+    break;
+    case "View all employees":
+    viewEmployees();
+    break;
+    case "Add a new department":
+    addDepartment();
+    break;
+    case "Add a new role":
+    addRole();
+    break;
+    case "Add a new employee":
+    addEmployee();
+    break;
+    case "Update employee roles":
+    employeeUpdate();
+    break;
+    case "Exit":
+    connection.end();
+    break;
+    }
+    });
+    };
+    
